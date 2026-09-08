c9b312ce4b5348955f7a6fd06adf4d7c4431fd50
Zaire <zairesevillano@gmail.com>
2026-09-08 18:43:22 +0800

    analytics fix

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import App from './App';
 import './index.css';
 import { Analytics } from '@vercel/analytics/react';
 import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <>
@@ -12,7 +11,6 @@ createRoot(document.getElementById('root')).render(
     <Analytics />
   </>
 );
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
