790269351616a15143fd0a23cbcd6aa9f342657b
Zaire <zairesevillano@gmail.com>
2026-09-08 18:40:10 +0800

    analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { Analytics } from '@vercel/analytics/react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+
+createRoot(document.getElementById('root')).render(
+  <>
+    <App />
+    <Analytics />
+  </>
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
